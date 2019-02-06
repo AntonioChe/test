29cf1a43ea03f67b8fb07e50a3d0d06f3731e412
anton <storm.antonio@hotmail.com>
2019-02-06 21:35:41 +0200

    second commit

--- chernenko/src/java/Test1.java
+++ chernenko/src/java/Test1.java
@@ -1,5 +1,14 @@
 public class Test1 {
     private String s;
+    private String ss;
+
+    public String getSs() {
+        return ss;
+    }
+
+    public void setSs(String ss) {
+        this.ss = ss;
+    }
 
     public String getS() {
         return s;
---
+++ chernenko/src/java/Test2.java
@@ -0,0 +1,11 @@
+public class Test2 {
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    private String d;
+}
