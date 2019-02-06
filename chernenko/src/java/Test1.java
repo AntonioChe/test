b4a8a8d029780d758264d849de56b6bd9c9eae91
anton <storm.antonio@hotmail.com>
2019-02-06 21:36:34 +0200

    Revert "second commit"
    This reverts commit 29cf1a43

--- chernenko/src/java/Test1.java
+++ chernenko/src/java/Test1.java
@@ -1,14 +1,5 @@
 public class Test1 {
     private String s;
-    private String ss;
-
-    public String getSs() {
-        return ss;
-    }
-
-    public void setSs(String ss) {
-        this.ss = ss;
-    }
 
     public String getS() {
         return s;
